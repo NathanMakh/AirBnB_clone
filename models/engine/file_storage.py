4d380dd6efa41d7f9ee0d72c58dfeadb1265c7c3
NathanMakh <nathankutete@yahoo.com>
2023-11-08 22:15:44 +0000

    push

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -1,20 +1,4 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-""" Class FileStorage """
-from json import dump, load, dumps
-from os.path import exists
-from models import base_model, user, place, state, city, amenity, review
-
-BaseModel = base_model.BaseModel
-User = user.User
-Place = place.Place
-State = state.State
-City = city.City
-Amenity = amenity.Amenity
-Review = review.Review
-name_class = ["BaseModel", "City", "State",
-              "Place", "Amenity", "Review", "User"]
-=======
 """File Storage Class"""
 
 
