e9e93d25556cfc7d98b7988249224910feb35690
NathanMakh <106968587+NathanMakh@users.noreply.github.com>
2023-11-09 14:57:56 +0300

    __init__.py

--- models/__init__.py
+++ models/__init__.py
@@ -1,21 +1,15 @@
-import os
+#!/usr/bin/python3
+""" FileStorage instance """
+
+
+from models.engine.file_storage import FileStorage
 from models.base_model import BaseModel
-from models.amenity import Amenity
-from models.city import City
-from models.place import Place
-from models.review import Review
-from models.state import State
 from models.user import User
+from models.review import Review
+from models.place import Place
+from models.city import City
+from models.amenity import Amenity
+from models.state import State
 
-"""CNC - dictionary = { Class Name (string) : Class Type }"""
-
-if os.environ.get('HBNB_TYPE_STORAGE') == 'db':
-    from models.engine import db_storage
-    CNC = db_storage.DBStorage.CNC
-    storage = db_storage.DBStorage()
-else:
-    from models.engine import file_storage
-    CNC = file_storage.FileStorage.CNC
-    storage = file_storage.FileStorage()
-
+storage = FileStorage()
 storage.reload()
